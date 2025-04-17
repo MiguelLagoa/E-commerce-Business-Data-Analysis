75c4f363c4938e9110078a8a5489d806f39918a3
MiguelLagoa <joao_lagoa_97@hotmail.com>
2025-04-18 00:51:17 +0100

    Updating file

--- data_loading.py
+++ data_loading.py
@@ -3,9 +3,8 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
 
-
+#load environment details from a local .env file 
 load_dotenv()
-
 
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
@@ -13,14 +12,15 @@ db_host = os.getenv("DB_HOST")
 db_port = os.getenv("DB_PORT")
 db_name = os.getenv("DB_NAME")
 
-# Step 2: Connect to RDS PostgreSQL
+# Connect to RDS PostgreSQL using SQLalchemy engine 
 engine = create_engine(f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
 
-# Step 3: Load your CSV
+# Load CSV. 
+# CSV had special characters so had to use 'ISO-8859-1' encoding 
 df = pd.read_csv('data.csv', encoding  = 'ISO-8859-1')
 
-# Step 4: Upload to database
+# Upload the dataframe to the PostgreSQL database under alias of "raw_data". If file already exists it is updated.
 df.to_sql('raw_data', engine, if_exists='replace', index=False)
 
-print("✅ Upload successful!")
+print( "Done")
 
